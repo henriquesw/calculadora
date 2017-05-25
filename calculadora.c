e1fdec408ab91bf97b837bfab3a832c0a0f6e819
henriquesw <henriqueswisniewski@gmail.com>
2017-05-25 19:05:35 -0300

    Adição da função soma

--- calculadora.c
+++ calculadora.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int soma () {}
+int soma (int a, int b) {
+	return a+b;
+}
 int subtrai () {}
 int mumltiplica () {}
 int divisao () {}
 
 int main(){
-	printf("Hello world!");
+	printf("Hello world!\n");
+	printf("Soma: %d\n", soma(10, 5));
 }
