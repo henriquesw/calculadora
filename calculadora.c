fe16cff2ca81e67326340a761c7c2013ab5d6c97
henriquesw <henriqueswisniewski@gmail.com>
2017-05-25 19:43:21 -0300

    Função divisão

--- calculadora.c
+++ calculadora.c
@@ -12,11 +12,14 @@ int subtrai (int a, int b) {
 int mumltiplica (int a, int b) {
 	return a*b;
 }
-int divisao () {}
+int divisao (int a, int b) {
+	return a/b;
+}
 
 int main(){
 	printf("Hello world!\n");
 	printf("Soma: %d\n", soma(10, 5));
 	printf("Subtração: %d\n", subtrai(10, 5));
 	printf("Multiplica: %d\n", multiplica(10,5));
+	printf("Divisão: %d\n", divisao(10, 5));
 }
